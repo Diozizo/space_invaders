c9b8b8877641b5538aa879e85a4b51ac2400138b
Diozizo <diaeddinemerad@gmail.com>
2025-12-29 12:59:02 +0100

    ncurses

---
+++ 3rdParty/ncurses/misc/run_tic.sed
@@ -0,0 +1,1 @@
+s%/usr/share/tabset%/home/zizou/studies/codes/c_cpp/projects/space_invaders/3rdParty/ncurses/build/share/tabset%g
--- clean.sh
+++ clean.sh
@@ -3,5 +3,6 @@
 rm -rf 3rdParty/SDL3-3.2.24/build
 rm -rf 3rdParty/SDL3_image-3.2.4/build
 rm -rf 3rdParty/SDL3_gfx-1.0.1/build
+rm -rf 3rdParty/ncurses/build
 
 make clean
